810e2d29d47016bae178022d41d1ce077e084d56
Rohan-154 <86555869+Rohan-154@users.noreply.github.com>
2021-08-07 14:42:12 +0530

    update: added sections to the quiz

---
+++ calcAngle.js
@@ -0,0 +1,13 @@
+const wrap= document.querySelectorAll(".wrapUp");
+const area= document.querySelectorAll(".area");
+const inputOption= document.querySelectorAll(".entryBox");
+const radios = document.forms[0].elements;
+const option1= document.querySelectorAll(".option1");
+const option2= document.querySelectorAll(".option2");
+const option3= document.querySelectorAll(".option3");
+const error= document.querySelector(".error");
+
+
+
+
+
